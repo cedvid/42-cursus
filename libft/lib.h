31608414d4996ed49009d8c895525382c2c7aa85
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-10 13:24:38 +0100

    upload new files

---
+++ libft/lib.h
@@ -0,0 +1,5 @@
+int	ft_isalpha(char c);
+int	ft_isdigit(char c);
+int	ft_strlen(char *s);
+int	ft_isalnum(char c);
+int	ft_isascii(char c);
---
+++ libft/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lib.h"
+
+int	main(void)
+{
+	printf("¡ returns %d\n", ft_isascii('¡'));
+	printf("a returns %d\n", ft_isdigit('a'));	
+	return 0;
+}	
