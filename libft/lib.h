131f7bf20c50c98b6175519918da42283987f32a
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-10 20:27:20 +0100

    update files

--- libft/lib.h
+++ libft/lib.h
@@ -1,5 +1,6 @@
-int	ft_isalpha(char c);
-int	ft_isdigit(char c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 int	ft_strlen(char *s);
-int	ft_isalnum(char c);
-int	ft_isascii(char c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int ft_isprint(int c);
--- libft/main.c
+++ libft/main.c
@@ -3,7 +3,28 @@
 
 int	main(void)
 {
-	printf("¡ returns %d\n", ft_isascii('¡'));
-	printf("a returns %d\n", ft_isdigit('a'));	
+	printf("isalnum\n");
+	printf("@ returns %d\n", ft_isalnum('@'));
+	printf("5 returns %d\n", ft_isalnum('5'));
+	printf("a returns %d\n", ft_isalnum('a'));
+	printf("isalpha\n");
+	printf("@ returns %d\n", ft_isalpha('@'));
+	printf("5 returns %d\n", ft_isalpha('5'));
+	printf("a returns %d\n", ft_isalpha('a'));
+	printf("isascii\n");
+	printf("@ returns %d\n", ft_isascii('@'));
+	printf("5 returns %d\n", ft_isascii('5'));
+	printf("a returns %d\n", ft_isascii('a'));
+	printf("isdigit\n");
+	printf("@ returns %d\n", ft_isdigit('@'));
+	printf("5 returns %d\n", ft_isdigit('5'));
+	printf("a returns %d\n", ft_isdigit('a'));
+	printf("isprintf\n");
+	printf("@ returns %d\n", ft_isprint('@'));
+	printf("5 returns %d\n", ft_isprint('5'));
+	printf("a returns %d\n", ft_isprint('a'));
+	printf("strlen\n");
+	printf("hello has %d letters\n", ft_strlen("hello"));
+	printf("helloooooooooooooooooooo hey has %d letters\n", ft_strlen("helloooooooooooooooooooo hey"));
 	return 0;
 }	
