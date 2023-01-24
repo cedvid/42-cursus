d2468091e3409cf1b70c809c69547b3bf70eda71
cedvid <cedricvidot@gmail.com>
2023-01-24 15:52:44 +0100

    Fix strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -13,12 +13,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (s1[i] == s2[i] && i < n)
+	while (i < n && (s1[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
