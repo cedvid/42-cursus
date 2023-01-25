b3ffba4fdc641982c1425ff8d3136d73f8a93736
cedvid <cedricvidot@gmail.com>
2023-01-25 21:54:01 +0100

    modif split and strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -15,13 +15,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	unsigned int	i;
 	unsigned int	dst_len;
-	unsigned int	src_len;
 
 	i = 0;
 	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (!src || !*src)
-		return (dst_len);
 	while (src[i] && i < size - dst_len -1)
 	{
 		dst[dst_len + i] = src[i];
