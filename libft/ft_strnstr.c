00a4ff49c85510abe7dc13dd465d776911d18d3e
cedvid <cedricvidot@gmail.com>
2023-01-25 11:01:17 +0100

    Add strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -10,16 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h> //remove later
 
 char *ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
 	if (!little)
-		return (big);
-	while (i <= len)
+		return ((char *)big);
+	while (big[i] && i < len)
 	{
-		
+		j = 0;
+		while (big[i + j] == little[j] && i < len)
+		{
+			if (little[j + 1] == '\0')
+				return ((char *)&big[i]);
+			j++;
+		}
+		i++;
 	}
+	return (NULL);
 }
+
+//testing purpose
+int main(void)
+{
+	char str[6] = "hello";
+	char str2[4] = "lo";
+	printf("the return value is %s\n", ft_strnstr(str, str2, 5));
+	return (0);
+}
+
+//      The strnstr() function locates the	first occurrence of the	null-termi-
+//      nated string little in the	string big, where not more than	len characters
+//      are searched.  Characters that appear after a `\0'	character are not
+//      searched.	Since the strnstr() function is	a FreeBSD specific API,	it
+//      should only be used when portability is not a concern.
+
+// RETURN VALUES
+//      If	little is an empty string, big is returned; if little occurs nowhere
+//      in	big, NULL is returned; otherwise a pointer to the first	character of
+//      the first occurrence of little is returned.
