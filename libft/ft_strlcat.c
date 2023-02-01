f7192baf09326e4ce980c0f3e810930077adb20a
cedvid <cedricvidot@gmail.com>
2023-02-01 10:02:16 +0100

    Modif strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -10,50 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-// #include <stdio.h>
-// #include <string.h>
-// #include <bsd/string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	dst_len;
-	unsigned int	src_len;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	len;
+	size_t	i;
 
 	i = 0;
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	while (src[i] && i < size - dst_len -1)
+	len = dst_len + src_len;
+	if (size <= dst_len)
+		return (size + src_len);
+	while (src[i] != '\0' && i < size - dst_len -1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	if (size < dst_len)
-		return (size + src_len);
-	else
-		return (dst_len + i);
+	return (len);
 }
-
-// int main(void)
-// {
-// 	char dest[20] = "hello";
-// 	char dest2[20] = "hello";
-	
-// 	// printf("return value is %zu\n\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 15));
-// 	// printf("real return value is %zu\n\n", strlcat(dest2, "lorem ipsum dolor sit amet", 15));
-// 	// printf("return value is %zu\n\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 0));
-// 	// printf("real return value is %zu\n\n", strlcat(dest2, "lorem ipsum dolor sit amet", 0));
-// 	// printf("return value is %zu\n\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 1));
-// 	// printf("real return value is %zu\n\n", strlcat(dest2, "lorem ipsum dolor sit amet", 1));
-// 	// printf("return value is %zu\n\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 5));
-// 	// printf("real return value is %zu\n\n", strlcat(dest2, "lorem ipsum dolor sit amet", 5));
-// 	printf("\nreturn value is %zu\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 6));
-// 	printf("real return value is %zu\n", strlcat(dest2, "lorem ipsum dolor sit amet", 6));
-// 	// printf("return value is %zu\n\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 15));
-// 	// printf("real return value is %zu\n\n", strlcat(dest2, "lorem ipsum dolor sit amet", 15));
-
-// 	printf("\ndest is:%s\n", dest);
-// 	printf("\ndest2 is:%s\n", dest2);
-// }
-		
