80c3b18f53a37d76281ea60da6f6a5ac207f71d0
cedvid <cedricvidot@gmail.com>
2023-02-02 12:10:44 +0100

    Modif ft_strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,57 +12,51 @@
 #include "libft.h"
 #include <stdio.h>
 
-int	ft_strlen(const char *s)
-{
-	int	i;
+// int	ft_strlen(const char *s)
+// {
+// 	int	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned int	i;
+// void	*ft_memcpy(void *dest, const void *src, size_t n)
+// {
+// 	unsigned int	i;
 
-	i = 0;
-	while (i < n)
-	{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dest);
-}
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		((char *)dest)[i] = ((char *)src)[i];
+// 		i++;
+// 	}
+// 	return (dest);
+// }
 
-int check_set(char c, char const *set)
-{
-    int i;
-
-    i = 0;
-    while (set[i] != '\0')
-    {
-        if (set[i] == c)
-            return (1);
-        i++;
-    }
-    return (0);
-}
+// int is_set(char const *set, char c)
+// {
+//     while (*set)
+//     {
+//         if (*set == c)
+//             return (1);
+//         set++;
+//     }
+//     return (0);
+// }
 
 char    *ft_strtrim(char const *s1, char const *set)
 {
-    int i;
-    int start;
-    int end;
-    char    *str;
-    char    *new;
+    char    *beg;
+    char    *end;
+    char    *trim_str;
 
-    i = 0;
-    start = 0;
-    end = ft_strlen(s1);
-    str = (char *) s1;
-    while ((check_set(str[i], set)) == 1)
-        i++;
+    beg = (char *) s1;
+    end = beg + ft_strlen(s1) - 1;
+    while (is_set(set, *beg))
+        beg++;
+    
     start = i;
     i = 0;
     while ((check_set(str[--end], set)) == 1)
@@ -72,8 +66,8 @@ char    *ft_strtrim(char const *s1, char const *set)
     return (new);
 }
 
-int main(void)
-{
-    printf("the trimmed string: %s\n", ft_strtrim("lorem ipsum dolor sit amet", "l"));
-    return 0;
-}
+// int main(void)
+// {
+//     printf("the trimmed string: %s\n", ft_strtrim("lorem ipsum dolor sit amet", "l"));
+//     return 0;
+// }
