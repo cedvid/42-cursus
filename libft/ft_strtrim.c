e21eea69892b797f1c4c9a3c34b1e66c8fa63d3c
cedvid <cedricvidot@gmail.com>
2023-02-03 10:45:14 +0100

    Fix strstrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -10,64 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
-// int	ft_strlen(const char *s)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (s[i] != '\0')
-// 		i++;
-// 	return (i);
-// }
-
-// void	*ft_memcpy(void *dest, const void *src, size_t n)
-// {
-// 	unsigned int	i;
-
-// 	i = 0;
-// 	while (i < n)
-// 	{
-// 		((char *)dest)[i] = ((char *)src)[i];
-// 		i++;
-// 	}
-// 	return (dest);
-// }
-
-// int is_set(char const *set, char c)
-// {
-//     while (*set)
-//     {
-//         if (*set == c)
-//             return (1);
-//         set++;
-//     }
-//     return (0);
-// }
+static int is_set(char const *set, char c)
+{
+    while (*set)
+    {
+        if (*set == c)
+            return (1);
+        set++;
+    }
+    return (0);
+}
 
 char    *ft_strtrim(char const *s1, char const *set)
 {
-    char    *beg;
+    char    *start;
     char    *end;
+    int     len;
     char    *trim_str;
+    int     i;
 
-    beg = (char *) s1;
-    end = beg + ft_strlen(s1) - 1;
-    while (is_set(set, *beg))
-        beg++;
-    
-    start = i;
     i = 0;
-    while ((check_set(str[--end], set)) == 1)
+    start = (char *) s1;
+    end = start + ft_strlen(s1) - 1;
+    while (is_set(set, *start))
+        start++;
+    while (is_set(set, *end) && end > start)
+        end--;
+    len = end - start + 1;
+    trim_str = (char *) malloc(len + 1);
+    if (!trim_str)
+        return (NULL);
+    while (i < len)
+    {
+        trim_str[i] = start[i];
         i++;
-    new = (char *)malloc(sizeof(char) * (end - start) + 1);
-    ft_memcpy(new, &str[start], (end - start + 1));
-    return (new);
+    }
+    trim_str[len] = '\0';
+    return (trim_str);
 }
-
-// int main(void)
-// {
-//     printf("the trimmed string: %s\n", ft_strtrim("lorem ipsum dolor sit amet", "l"));
-//     return 0;
-// }
