73b27b1cc24e13d66a9302b856e3ff3c35b70d7c
cedvid <cedricvidot@gmail.com>
2023-02-03 10:53:51 +0100

    Remove sizeof from malloc

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,17 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-// #include <stdio.h>
-
-int	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
 
 char    *ft_strjoin(char const *s1, char const *s2)
 {
@@ -30,7 +19,7 @@ char    *ft_strjoin(char const *s1, char const *s2)
 
     i = 0;
     j = 0;
-    str = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+    str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
     if (!str)
         return (NULL);
     while (s1[i] != '\0')
@@ -47,11 +36,3 @@ char    *ft_strjoin(char const *s1, char const *s2)
     str[i] = '\0';
     return (str);
 }
-
-// int main(void)
-// {
-//     char str[6] = "hello";
-//     char str2[6] = "aloha";
-//     printf("the new str is: %s\n", ft_strjoin(str, str2));
-//     return 0;
-// }
