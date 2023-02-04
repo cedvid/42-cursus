6540ddc4dcf0edd9552d71b48573ddb46e9df12a
cedvid <cedricvidot@gmail.com>
2023-02-04 20:41:52 +0100

    Upload ftputendl

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -9,7 +9,18 @@
 /*   Updated: 2023/01/16 12:47:20 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+#include <unistd.h>
+
 void    ft_putendl_fd(char *s, int fd)
 {
-    
+    int i;
+
+    i = 0;
+    while (s[i] != '\0')
+    {
+        write(fd, &s[i], 1);
+        i++;
+    }
+    write(fd, "\n", 1);
 }
