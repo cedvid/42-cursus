e88c27196ed6807321740e549db841c431bc7da7
cedvid <cedricvidot@gmail.com>
2023-02-10 09:31:11 +0100

    Add ft_putnbr_fd to makefile and libft

--- libft/libft.h
+++ libft/libft.h
@@ -34,6 +34,7 @@ void    *ft_calloc(size_t nmemb, size_t size);
 void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
 char *ft_itoa(int n);
 
 #endif
