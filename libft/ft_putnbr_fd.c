72697b5aa63448d975e34393c972eebafbdacf3f
cedvid <cedricvidot@gmail.com>
2023-02-10 09:29:06 +0100

    Add ft_putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -9,7 +9,49 @@
 /*   Updated: 2023/01/16 12:47:39 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+// void	ft_putchar_fd(char c, int fd)
+// {
+// 	write(fd, &c, 1);
+// }
+
+// void    ft_putstr_fd(char *s, int fd)
+// {
+//     int i;
+
+//     i = 0;
+//     while (s[i] != '\0')
+//     {
+//         write(fd, &s[i], 1);
+//         i++;
+//     }
+// }
+
 void    ft_putnbr_fd(int n, int fd)
 {
-    
+    if (n == -2147483648)
+    {
+        ft_putstr_fd("-2147483648", fd);
+        return;
+    }
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n *= -1;
+    }
+    if (n <= 9)
+        ft_putchar_fd(n + '0', fd);
+    else
+    {
+        ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10, fd);
+    }
+    return;
 }
+
+// int main(void)
+// {
+//     ft_putnbr_fd(2147483647, 1);
+//     return 0;
+// }
