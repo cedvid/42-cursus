0de67cdc48168a40bece3dc9781395596fc5dae8
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-02-13 15:16:29 +0100

    Add function lstlast

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvidot <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 15:02:19 by cvidot            #+#    #+#             */
+/*   Updated: 2023/02/13 15:06:37 by cvidot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdio.h>
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*node;
+
+	node = lst;
+	while (node)
+	{
+		if (node->next == NULL)
+			return (node);
+		else
+			node = node->next;
+	}
+}
+/*
+int main(void)
+{
+    t_list *new_node = ft_lstnew("42");
+	t_list *node2 = ft_lstnew("43");
+	t_list *node3 = ft_lstnew("44");
+	t_list *list = NULL;
+
+    ft_lstadd_front(&list, node3);
+	ft_lstadd_front(&list, node2);
+    ft_lstadd_front(&list, new_node);
+
+	printf("First node address: %p\n", new_node);
+	printf("Second node address: %p\n", node2);
+	printf("Last node address: %p\n", node3);
+
+    printf("\nLast node is: %p\n", ft_lstlast(list));
+    return (0);
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cvidot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:16:55 by cvidot            #+#    #+#             */
-/*   Updated: 2023/02/13 14:48:36 by cvidot           ###   ########.fr       */
+/*   Updated: 2023/02/13 15:13:40 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -59,5 +59,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
