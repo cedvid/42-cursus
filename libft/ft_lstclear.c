d9cf21eacc7ac28c07ff7ab95510f1d22fbe92d8
cedvid <cedricvidot@gmail.com>
2023-02-15 12:22:03 +0100

    Add lstclear function

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -10,10 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*node;
 
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		node = *lst;
+		*lst = node->next;
+		ft_lstdelone(node, del);
+	}
+}
+
+int main(void)
+{
+    t_list *new_node = ft_lstnew("42");
+	t_list *node2 = ft_lstnew("43");
+	t_list *node3 = ft_lstnew("44");
+	t_list *list = NULL;
+
+    ft_lstadd_front(&list, node3);
+	ft_lstadd_front(&list, node2);
+    ft_lstadd_front(&list, new_node);
+
+    printf("Original linked list:\n");
+    while (list != NULL)
+	{
+        printf("%s\n", (char *)list->content);
+        list = list->next;
+    }
+
+    ft_lstclear(&list, free);
+
+    printf("Cleared linked list:\n");
+    while (list != NULL)
+	{
+        printf("%s\n", (char *)list->content);
+        list = list->next;
+    }
+
+    return 0;
 }
 
 /*Function name ft_lstclear
