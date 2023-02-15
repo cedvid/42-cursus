5b39f114b880c39f32eb357963265c0c9ef86132
cedvid <cedricvidot@gmail.com>
2023-02-15 15:22:07 +0100

    Add lstiter function

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -10,11 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*node;
 
+	if (!lst)
+		return ;
+	node = lst;
+	while (node)
+	{
+		(*f)(node->content);
+		node = node->next;
+	}
 }
+/*
+void print_node(void *content)
+{
+    printf("%s\n", (char *)content);
+}
+
+int main(void)
+{
+    t_list *new_node = ft_lstnew("42");
+	t_list *node2 = ft_lstnew("43");
+	t_list *node3 = ft_lstnew("44");
+	t_list *list = NULL;
+
+    ft_lstadd_front(&list, node3);
+	ft_lstadd_front(&list, node2);
+    ft_lstadd_front(&list, new_node);
+
+	ft_lstiter(list, print_node);
+
+    return (0);
+}*/
 
 /*Function name ft_lstiter
 Prototype void ft_lstiter(t_list *lst, void (*f)(void *));
