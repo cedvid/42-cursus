564c54eb3bd735bb5e1e284cca247ee6095b867a
cedvid <cedricvidot@gmail.com>
2023-02-15 22:48:52 +0100

    Include header and change logic

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -9,13 +9,11 @@
 /*   Updated: 2023/01/10 13:06:09 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
 int	ft_isalnum(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	else if (c >= 65 && c <= 90)
-		return (1);
-	else if (c >= 97 && c <= 122)
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
