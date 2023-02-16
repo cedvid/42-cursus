bb1e7c60fdf17e401391aa58dc887f6a5952ea0a
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-02-16 12:50:02 +0100

    Merge two conditions into one

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -13,9 +13,7 @@
 
 int	ft_isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	return (0);
 }
