d781f6d6bd7682accfb5c7fda28966afe9ad2934
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-02-16 13:57:20 +0100

    Add test main

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -20,4 +21,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	ft_memcpy(dest, tmp, n);
 	free(tmp);
 	return (dest);
-}
+}/*
+int main(void)
+{
+	char str[] = "hello world";
+	printf("%s\n", ft_memmove(str, str+6, 5));
+}*/
