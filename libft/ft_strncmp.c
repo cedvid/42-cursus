474e62b1b6db93eb96584e1d655bc78898fb2650
cedvid <cedricvidot@gmail.com>
2023-02-20 10:07:12 +0100

    Remove unnecessary condition

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -16,15 +16,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if ((*s1 == '\0' || *s2 == '\0') && n > 0)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	while (i < n)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	if (n == 0)
+		return (0);
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 /*The strncmp() function compares only the first 
 (at most) n bytes of s1 and s2. 
